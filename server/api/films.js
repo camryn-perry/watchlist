8ecfe9551d391c5de130c1a70e23763c4c858792
Camryn Perry <camrynpaigeperry@gmail.com>
2019-11-14 15:23:21 -0500

    feat: search by keyword films api route

--- server/api/films.js
+++ server/api/films.js
@@ -4,7 +4,10 @@ const {API_KEY} = require('../../secrets');
 const client = new imdb.Client({apiKey: API_KEY});
 
 //have to parse incoming data to match parameter names. I.E, north by northwest must be north+by+northwest. Helper func?
+//if response true, send back
+//post/put methods add to own database if user wants to mark as favorite
 
+//should eventually be homepage of all movies in db
 router.get('/', async (req, res, next) => {
   try {
     const movie = await client.get({name: 'Batman'});
@@ -14,5 +17,19 @@ router.get('/', async (req, res, next) => {
     next(err);
   }
 });
+
+router.get('/search', async (req, res, next) => {
+  try {
+    const search = await client.search({name: req.body.title});
+    res.json(search.results);
+  } catch (err) {
+    console.log(err.message);
+    next(err);
+  }
+});
+
+//other routes -- add to watchlist creates new movie obj that persists in database
+//remove -- removes from watchlist and removes from database
+//add to favorites/ or rating. saves a new rating value column from 1-5. Could be useful for recommendation portion
 
 module.exports = router;
