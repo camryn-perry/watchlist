ecf8777dd5a64632d0fe0e2884d106de397606cc
Camryn Perry <camrynpaigeperry@gmail.com>
2019-11-14 22:26:31 -0500

    feat: added thunks to film reducer

--- client/store/film.js
+++ client/store/film.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import {runInNewContext} from 'vm';
 
 //INITIAL STATE
 const intitialState = {
@@ -15,6 +16,7 @@ const ADD_FILM = 'ADD_FILM';
 const REMOVE_FILM = 'REMOVE_FILM';
 
 //action creators
+//need one for search route and advanced search
 const loadedFilms = allFilms => ({type: LOAD_FILMS, allFilms});
 const gotFilm = film => ({type: GET_FILM, film});
 const gotWatchlist = watchlist => ({type: GET_WATCHLIST, watchlist});
@@ -22,11 +24,51 @@ const addedFilm = film => ({type: ADD_FILM, film});
 const removedFilm = () => ({type: REMOVE_FILM});
 
 //thunk creators
-export const loadFilms = {};
-export const getFilm = {};
-export const getWatchlist = {};
-export const addFilm = {};
-export const removeFilm = {};
+export const loadFilms = () => async dispatch => {
+  try {
+    const {data} = await axios.get('/api/films');
+    dispatch(loadedFilms(data));
+  } catch (err) {
+    console.error(err);
+  }
+};
+// export const getFilm = (filmId) => async dispatch => {
+//   try {
+//     const {data} = await axios.get('api/film')
+//   } catch (err) {
+//     console.error(err);
+//   };
+// };
+export const getWatchlist = film => async dispatch => {
+  try {
+    const {data} = await axios.get(`api/films/watchlist/${userId}`);
+    dispatch(gotWatchlist(data));
+  } catch (err) {
+    console.error(err);
+  }
+};
+export const addFilm = film => async dispatch => {
+  try {
+    const user = await axios.get('auth/me').data;
+    const {data} = await axios.put(`api/films/watchlist/add/${user.id}`, {
+      filmId: film
+    });
+    dispatch(addedFilm(data));
+  } catch (err) {
+    console.error(err);
+  }
+};
+export const removeFilm = film => async dispatch => {
+  try {
+    const user = await axios.get('auth/me').data;
+    const {data} = await axios.put(`api/films/watchlist/remove/${user.id}`, {
+      filmId: film
+    });
+    dispatch(removedFilm(data));
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 /**
  * REDUCER
--- client/store/index.js
+++ client/store/index.js
@@ -3,8 +3,9 @@ import {createLogger} from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import user from './user';
+import film from './film';
 
-const reducer = combineReducers({user});
+const reducer = combineReducers({user, film});
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 );
